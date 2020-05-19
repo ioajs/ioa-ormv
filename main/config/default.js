b922627a7ede49524dbf6d74f21a539dd56acd6b
lixiang <lixiangio@qq.com>
2020-05-19 10:49:35 +0800

    9.8.9

--- main/config/default.js
+++ main/config/default.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-   "@ioa/ormv": {
-      "host": "localhost",
-      "port": 5532,
-      "database": "test",
-      "username": "postgres",
-      "password": 'M2Idiftre&34FS',
-      "logger": true
-   }
+   // "@ioa/ormv": {
+   //    "host": "localhost",
+   //    "port": 5532,
+   //    "database": "test",
+   //    "username": "postgres",
+   //    "password": 'M2Idiftre&34FS',
+   //    "logger": true
+   // }
 }
